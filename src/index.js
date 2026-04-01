4a7a2b575b02703a5b38b82e039304a5f876dfd5
Mohamed Ben Slama <MedBenSlama@users.noreply.github.com>
2026-04-01 16:14:11 +0100

    adding files v2

--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:5174"],
+    origin: ["https://tunichatt.onrender.com", "https://tunichatt.onrender.com"],
     credentials: true,
   })
 );
