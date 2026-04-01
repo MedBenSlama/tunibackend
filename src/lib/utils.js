fd9ca8874df98e17ca1bda1029529cabeb916811
Mohamed Ben Slama <MedBenSlama@users.noreply.github.com>
2026-04-01 17:07:17 +0100

    adding files v4

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,16 +1,17 @@
 import jwt from "jsonwebtoken";
 
+// Generates JWT and sets it as an HTTP-only cookie
 export const generateToken = (userId, res) => {
   const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
     expiresIn: "7d",
   });
 
   res.cookie("jwt", token, {
-    maxAge: 7 * 24 * 60 * 60 * 1000, // MS
-    httpOnly: true, // prevent XSS attacks cross-site scripting attacks
-    sameSite: "strict", // CSRF attacks cross-site request forgery attacks
-    secure: process.env.NODE_ENV !== "development",
+    maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+    httpOnly: true,                   // frontend JS cannot access
+    secure: process.env.NODE_ENV === "production", // HTTPS only in prod
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax", // cross-site cookies
   });
 
-  return token;
+  return token; // optional
 };
